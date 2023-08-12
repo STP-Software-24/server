cdf80cca2a72b0f837313d338c3ca8624c4c7380
Omar Fahmy <Ofahmy1234@gmail.com>
2023-08-12 14:22:47 +0300

    fix: connection string hidden

--- src/services/database.ts
+++ src/services/database.ts
@@ -1,9 +1,16 @@
 import { Client } from 'pg';
+import { config } from '../config/config';
 
 const connectionString =
     'postgres://ofahmy1234:HcSZV9ulh4tL@ep-sparkling-scene-15350032.us-east-2.aws.neon.tech/neondb?sslmode=require';
+
+export function getDbConnectionString() {
+    while (!config.DB_CONNECTION_STRING) {}
+    return config.DB_CONNECTION_STRING;
+}
+
 export const dbClient = new Client({
-    connectionString: connectionString,
+    connectionString: config.DB_CONNECTION_STRING,
 });
 export async function dbConnect() {
     await dbClient.connect();
