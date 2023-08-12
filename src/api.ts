acedd4275d0095408433bbf48ea97326cf2110e5
Omar Fahmy <Ofahmy1234@gmail.com>
2023-08-12 19:15:09 +0300

    feat: setup cors

--- src/api.ts
+++ src/api.ts
@@ -1,8 +1,9 @@
 import express from 'express';
 import router from './router/router';
+import cors from 'cors';
 
 const api = express();
-
+api.use(cors());
 api.use(express.json());
 api.use('/', router);
 
