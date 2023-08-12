79d31a86da7a3bc46a9453a37d0f88f755699a91
Omar Fahmy <Ofahmy1234@gmail.com>
2023-08-12 16:33:48 +0300

    feat: add health-check

--- src/router/router.ts
+++ src/router/router.ts
@@ -1,7 +1,17 @@
 import { Router } from 'express';
 import memberRecruitmentRouter from './member.recruitment.router';
+import { dbClient } from '../services/database';
 
 const router = Router();
 
+router.get('/health-check',async (req, res) => {
+    try {
+        await dbClient.query('SELECT NOW()');
+        res.status(200).json({ message: 'OK' });
+    } catch (error) {
+        res.status(500).json({ message: 'Internal Server Error' });
+    }
+});
 router.use('/member-recruitment', memberRecruitmentRouter);
+
 export default router;
