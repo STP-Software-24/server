36dc7fa7469c3b7ebdb94afd4f50ece647758aba
Omar Fahmy <Ofahmy1234@gmail.com>
2023-08-13 13:08:02 +0300

    fix: injection vulnerability

--- src/model/members.recruitment.model.ts
+++ src/model/members.recruitment.model.ts
@@ -3,56 +3,59 @@ import { RecruitmentMember } from '../types/members.recruitment';
 
 export async function dbAddMember(member: RecruitmentMember) {
     const insertQuery = `
-    INSERT INTO members_recruitment (
-      department,
-      name,
-      mobile,
-      email,
-      areaofresidency,
-      university,
-      faculty,
-      graduationyear,
-      firstpreference,
-      secondpreference,
-      reasonforapplying,
-      previousexperience
-      ${member.q1 ? ', q1' : ''}
-      ${member.q2 ? ', q2' : ''}
-      ${member.q3 ? ', q3' : ''}
-      ${member.q4 ? ', q4' : ''}
-      ${member.q5 ? ', q5' : ''}
-      ${member.q6 ? ', q6' : ''}
-      ${member.subFirstPreference ? ', subfirstpreference' : ''}
-      ${member.subSecondPreference ? ', subsecondpreference' : ''}
+  INSERT INTO members_recruitment (
+    department,
+    name,
+    mobile,
+    email,
+    areaofresidency,
+    university,
+    faculty,
+    graduationyear,
+    firstpreference,
+    secondpreference,
+    reasonforapplying,
+    previousexperience,
+    q1,
+    q2,
+    q3,
+    q4,
+    q5,
+    q6,
+    subfirstpreference,
+    subsecondpreference
+  )
+  VALUES (
+    $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20
+  )
+`;
 
-    )
-    VALUES (
-      '${member.department}',
-      '${member.name}',
-      '${member.mobile}',
-      '${member.email}',
-      '${member.areaOfResidency}',
-      '${member.university}',
-      '${member.faculty}',
-      '${member.graduationYear}',
-      '${member.firstPreference}',
-      '${member.secondPreference}',
-      '${member.reasonForApplying}',
-      '${member.previousExperience}'
-      ${member.q1 ? `, '${member.q1}'` : ''}
-      ${member.q2 ? `, '${member.q2}'` : ''}
-      ${member.q3 ? `, '${member.q3}'` : ''}
-      ${member.q4 ? `, '${member.q4}'` : ''}
-      ${member.q5 ? `, '${member.q5}'` : ''}
-      ${member.q6 ? `, '${member.q6}'` : ''}
-      ${member.subFirstPreference ? `, '${member.subFirstPreference}'` : ''}
-      ${member.subSecondPreference ? `, '${member.subSecondPreference}'` : ''}
-    );
-  `;
+    const values = [
+        member.department,
+        member.name,
+        member.mobile,
+        member.email,
+        member.areaOfResidency,
+        member.university,
+        member.faculty,
+        member.graduationYear,
+        member.firstPreference,
+        member.secondPreference,
+        member.reasonForApplying,
+        member.previousExperience,
+        member.q1,
+        member.q2,
+        member.q3,
+        member.q4,
+        member.q5,
+        member.q6,
+        member.subFirstPreference,
+        member.subSecondPreference,
+    ];
 
-  console.log(insertQuery);
+    console.log(insertQuery, values);
     try {
-        return await dbClient.query(insertQuery);
+        return await dbClient.query(insertQuery, values);
     } catch (error) {
         throw new Error((error as Error).message);
     }
