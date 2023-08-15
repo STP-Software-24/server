6073522b2d7780e58582d57fba0a0ff0aa7ba5d8
Omar Fahmy <Ofahmy1234@gmail.com>
2023-08-15 18:25:51 +0300

    feat: change members field order

--- src/model/members.recruitment.model.ts
+++ src/model/members.recruitment.model.ts
@@ -4,25 +4,25 @@ import { RecruitmentMember } from '../types/members.recruitment';
 export async function dbAddMember(member: RecruitmentMember) {
     const insertQuery = `
   INSERT INTO members_recruitment (
-    department,
     name,
     mobile,
     email,
     areaofresidency,
     university,
     faculty,
+    department,
     graduationyear,
     firstpreference,
-    secondpreference,
-    reasonforapplying,
-    previousexperience,
+    subfirstpreference,
     q1,
     q2,
     q3,
     q4,
     q5,
     q6,
-    subfirstpreference,
+    reasonforapplying,
+    previousexperience,
+    secondpreference,
     subsecondpreference
   )
   VALUES (
@@ -31,25 +31,25 @@ export async function dbAddMember(member: RecruitmentMember) {
 `;
 
     const values = [
-        member.department,
         member.name,
         member.mobile,
         member.email,
         member.areaOfResidency,
         member.university,
         member.faculty,
+        member.department,
         member.graduationYear,
         member.firstPreference,
-        member.secondPreference,
-        member.reasonForApplying,
-        member.previousExperience,
+        member.subFirstPreference,
         member.q1,
         member.q2,
         member.q3,
         member.q4,
         member.q5,
         member.q6,
-        member.subFirstPreference,
+        member.reasonForApplying,
+        member.previousExperience,
+        member.secondPreference,
         member.subSecondPreference,
     ];
 
@@ -63,7 +63,30 @@ export async function dbAddMember(member: RecruitmentMember) {
 
 export async function dbGetAllMembersApplication() {
     const selectQuery = `
-    SELECT * FROM members_recruitment;
+    SELECT
+    id,
+    name,
+    department,
+    mobile,
+    email,
+    areaofresidency,
+    university,
+    faculty,
+    graduationyear,
+    firstpreference,
+    subfirstpreference,
+    q1,
+    q2,
+    q3,
+    q4,
+    q5,
+    q6,
+    reasonforapplying,
+    previousexperience,
+    secondpreference,
+    subsecondpreference
+  FROM members_recruitment
+  ORDER BY id; 
   `;
 
     try {
