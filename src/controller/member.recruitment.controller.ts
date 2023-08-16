b3d5bcb3757d1d0d626370b3be86c5919e1e9913
Omar Fahmy <Ofahmy1234@gmail.com>
2023-08-16 23:20:54 +0300

    feat: registeration email template

--- src/controller/member.recruitment.controller.ts
+++ src/controller/member.recruitment.controller.ts
@@ -1,12 +1,15 @@
 import { Request, Response } from 'express';
 import { dbAddMember, dbGetAllMembersApplication } from '../model/members.recruitment.model';
+import { sendRegisterationEmail } from '../utils/mail.templates';
+import { RecruitmentMember } from '../types/members.recruitment';
 
 //TODO: ADD DTO
 // TODO: ADD VALIDATION
 export async function registerMemberApplication(req: Request, res: Response) {
-    const member = req.body;
+    const member = req.body as RecruitmentMember;
     try {
         const addedMember = await dbAddMember(member);
+        await sendRegisterationEmail(member.email)
         res.json({ status: 'success', data: addedMember });
     } catch (error) {
         res.json({
