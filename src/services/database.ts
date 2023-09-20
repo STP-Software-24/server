769ca8d6550db2140c7cf66c485882a388a6739c
Omar Fahmy <Ofahmy1234@gmail.com>
2023-09-21 02:44:03 +0300

    feat: fix connection to prod db

--- src/services/database.ts
+++ src/services/database.ts
@@ -7,7 +7,13 @@ export const dbClient = new Client({
     port: 5432,
     password: config.PASSWORD,
     database: config.DATABASE,
+    ssl: {
+        rejectUnauthorized: true,
+    },
 });
+// export const dbClient = new Client({
+//     connectionString: process.env.PROD_DB_CONNECTION_STRING
+// });
 
 export async function dbConnect() {
     await dbClient.connect();
