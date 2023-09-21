9b7cc2252763e2308f5a86f5489ea476894b0508
Omar Fahmy <Ofahmy1234@gmail.com>
2023-09-21 13:56:23 +0300

    feat: string connection to db

--- src/services/database.ts
+++ src/services/database.ts
@@ -1,19 +1,19 @@
 import { Client } from 'pg';
 import config from '../config/config';
 
-export const dbClient = new Client({
-    host: config.HOST,
-    user: config.USER,
-    port: 5432,
-    password: config.PASSWORD,
-    database: config.DATABASE,
-    ssl: {
-        rejectUnauthorized: true,
-    },
-});
 // export const dbClient = new Client({
-//     connectionString: process.env.PROD_DB_CONNECTION_STRING
+//     host: config.HOST,
+//     user: config.USER,
+//     port: 5432,
+//     password: config.PASSWORD,
+//     database: config.DATABASE,
+//     ssl: {
+//         rejectUnauthorized: true,
+//     },
 // });
+export const dbClient = new Client({
+    connectionString: process.env.PROD_DB_CONNECTION_STRING
+});
 
 export async function dbConnect() {
     await dbClient.connect();
