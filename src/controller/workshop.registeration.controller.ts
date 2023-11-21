df5a2db513e4369a7f6804c7c11ceaa50dd134d4
Omar Fahmy <ofahmy1234@gmail.com>
2023-11-22 00:39:56 +0200

    fix: filter in update query

--- src/controller/workshop.registeration.controller.ts
+++ src/controller/workshop.registeration.controller.ts
@@ -66,7 +66,7 @@ export async function assignUniqueCode(req: Request, res: Response) {
             const uniqueCode = generateEightCharCode();
 
             await dbClient.query(
-                `UPDATE workshop_participants SET unique_code = '${uniqueCode}' WHERE id = ${tuple.phone_number}`,
+                `UPDATE workshop_participants SET unique_code = '${uniqueCode}' WHERE phone_number = ${tuple.phone_number}`,
             );
         }
 
